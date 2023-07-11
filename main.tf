d6485f56bda962ecc90e1913397c68d36aefb9c7
Michael Grassi <michael.g96@live.com.au>
2023-07-11 14:22:00 +1000

    Addressed tfsec concerns about s3 bucket.

--- main.tf
+++ main.tf
@@ -1,6 +1,26 @@
-resource "aws_s3_bucket" "b" {
+#tfsec:ignore:aws-s3-enable-versioning
+#tfsec:ignore:aws-s3-encryption-customer-key
+resource "aws_s3_bucket" "cloud_resume_site_bucket" {
   bucket = "tf-aws-mgrassi-cloud-resume-challenge"
-  acl    = "public"
+
+  server_side_encryption_configuration {
+    rule {
+      bucket_key_enabled = true
+
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "cloud_resume_site_bucket" {
+  bucket = aws_s3_bucket.cloud_resume_site_bucket.id
+
+  block_public_acls = true
+  block_public_policy = true 
+  ignore_public_acls = true
+  restrict_public_buckets = true
 }
 
 #TODO: prevent billshock in AWS
