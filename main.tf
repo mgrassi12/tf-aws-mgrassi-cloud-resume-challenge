1dccf34a8eb528698c85c838febd469a5b9e1255
Michael Grassi <michael.g96@live.com.au>
2023-07-11 14:28:10 +1000

    Added logging bucket and formatted tf files.

--- main.tf
+++ main.tf
@@ -3,12 +3,16 @@
 resource "aws_s3_bucket" "cloud_resume_site_bucket" {
   bucket = "tf-aws-mgrassi-cloud-resume-challenge"
 
+  logging {
+    target_bucket = aws_s3_bucket.cloud_resume_logging_bucket.id
+  }
+
   server_side_encryption_configuration {
     rule {
       bucket_key_enabled = true
 
       apply_server_side_encryption_by_default {
-        sse_algorithm     = "AES256"
+        sse_algorithm = "AES256"
       }
     }
   }
@@ -17,9 +21,38 @@ resource "aws_s3_bucket" "cloud_resume_site_bucket" {
 resource "aws_s3_bucket_public_access_block" "cloud_resume_site_bucket" {
   bucket = aws_s3_bucket.cloud_resume_site_bucket.id
 
-  block_public_acls = true
-  block_public_policy = true 
-  ignore_public_acls = true
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+#tfsec:ignore:aws-s3-enable-bucket-logging
+#tfsec:ignore:aws-s3-encryption-customer-key
+resource "aws_s3_bucket" "cloud_resume_logging_bucket" {
+  bucket = "tf-aws-mgrassi-cloud-resume-challenge"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      bucket_key_enabled = true
+
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "cloud_resume_logging_bucket" {
+  bucket = aws_s3_bucket.cloud_resume_logging_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
