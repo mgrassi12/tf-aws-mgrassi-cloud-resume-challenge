fe15f1dcc00669aeb9ed8d8a389b4a75d01831c1
Michael Grassi <michael.g96@live.com.au>
2023-07-11 15:22:37 +1000

    Added versioning and ownership controls for logging bucket.

--- main.tf
+++ main.tf
@@ -30,9 +30,13 @@ resource "aws_s3_bucket_public_access_block" "cloud_resume_site_bucket" {
 #tfsec:ignore:aws-s3-encryption-customer-key
 resource "aws_s3_bucket" "cloud_resume_logging_bucket" {
   bucket = "tf-aws-mgrassi-cloud-resume-challenge-logging"
+}
 
-  versioning {
-    enabled = true
+resource "aws_s3_bucket_versioning" "cloud_resume_logging_bucket" {
+  bucket = aws_s3_bucket.cloud_resume_logging_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
   }
 }
 
@@ -55,6 +59,14 @@ resource "aws_s3_bucket_public_access_block" "cloud_resume_logging_bucket" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_ownership_controls" "cloud_resume_logging_bucket" {
+  bucket = aws_s3_bucket.cloud_resume_logging_bucket.id
+
+  rule {
+    object_ownership = "ObjectWriter"
+  }
 }
 
 resource "aws_s3_bucket_acl" "cloud_resume_logging_bucket" {
