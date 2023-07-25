8bb759404eb77ac9c1c4e8f2d03ca411e8711964
Michael Grassi <michael.g96@live.com.au>
2023-07-25 14:20:09 +1000

    Added static page html files to s3 buckets.

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -25,6 +25,35 @@ resource "aws_s3_bucket_public_access_block" "cloud_resume_site_bucket" {
   restrict_public_buckets = true
 }
 
+resource "aws_s3_object" "index" {
+  bucket = aws_s3_bucket.cloud_resume_site_bucket.id
+  key    = "index.html"
+  source = "${var.environment}/src/index.html"
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("${var.environment}/src/index.html")
+}
+
+resource "aws_s3_object" "error" {
+  bucket = aws_s3_bucket.cloud_resume_site_bucket.id
+  key    = "error.html"
+  source = "${var.environment}/src/error.html"
+  etag = filemd5("${var.environment}/src/error.html")
+}
+
+resource "aws_s3_bucket_website_configuration" "cloud_resume_site_bucket" {
+  bucket = aws_s3_bucket.cloud_resume_site_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
 #tfsec:ignore:aws-s3-enable-bucket-logging
 #tfsec:ignore:aws-s3-encryption-customer-key
 resource "aws_s3_bucket" "cloud_resume_logging_bucket" {
--- nonprod/main.tf
+++ nonprod/main.tf
@@ -2,3 +2,7 @@ module "s3" {
   source = "../modules/s3"
   environment = "nonprod"
 }
+
+# TODO: Add nonprod branch in Github and setup branch protection 
+# TODO: Make a pretty README file. 
+# TODO: Actually deploy infrastructure for Cloud Resume Challenge.
